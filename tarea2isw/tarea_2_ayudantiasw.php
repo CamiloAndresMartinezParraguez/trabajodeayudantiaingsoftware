7ce62921362cb2ac60f44cfb7a6562ec9a2685a0
CamiloAndresMartinezParraguez <distorshow@hotmail.com>
2013-11-15 12:06:26 -0300

    comit1

---
+++ tarea2isw/tarea_2_ayudantiasw.php
@@ -0,0 +1,43 @@
+<!doctype html>
+<html lang="es">
+
+    <head>
+     <link rel="stylesheet" type="text/css" href="stylo.css">
+    <meta charset="utf-8">
+    <title>
+    Universidad Tecnologica Metropolitana
+    </title>
+    <link href="/img/favicon.ico" type="image/x-icon" rel="icon" />
+    <link href="/img/favicon.ico" type="image/x-icon" rel="shortcut icon" />
+    </head>
+    <body>
+    <span style="text-decoration:underline; font-family:arial; color:blue; font-size:22px;"> Trabajo de Ayudantía Ingeniería de Software Numero 2 </span> <br><br>
+    Frase de grupo:<br>
+    <?php
+     $variable= rand(1, 2);
+     if($variable==1){
+        echo "Bazinga!!!";
+     }
+     if ($variable==2){
+         echo "Haz el amor y no la guerra";
+    }   
+    ?>
+    <hr align="LEFT" size="1" width="300" color="Red" noshade> 
+    <br>Integrantes del Grupo: <br>
+    Nombre :Camilo Martinez Parraguez <br>
+    Edad : 23<br>
+    Carrera: Ing. informatica (UTEM)<br>
+    Hobby: Subir cerros<br>
+    Mail:<A HREF="martinez.camiloandres@gmail.com">martinez.camiloandres@gmail.com</A><br>
+    <A HREF="https://github.com/kmilorio"> Perfil GitHub de Camilo Martinez</A><br>
+    <img src="https://fbcdn-sphotos-d-a.akamaihd.net/hphotos-ak-frc3/1468805_602112096492417_142576635_n.jpg" "LINK" width="190" height="200"alt="kmilo" /<br><br>
+    <hr align="LEFT" size="1" width="300" color="Red" noshade> 
+    Nombre :Jhon Lopez <br>
+    Edad : 23<br>
+    Carrera: Ing. informatica (UTEM)<br>
+    Hobby:Guitarra<br>
+    Mail:<A HREF="jp.lopez.suarez@gmail.com">jp.lopez.suarez@gmail.com</A><br>
+    <A HREF=https://github.com/johnlopez"> Perfil GitHub de Jhon Lopez</A><br>
+    <img src="https://fbcdn-sphotos-c-a.akamaihd.net/hphotos-ak-ash3/584_4869008524476_844284668_n.jpg" "LINK" width="190" height="200"alt="John" /
+    <hr align="LEFT" size="1" width="300" color="Red" noshade>
+    </html>
